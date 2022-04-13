0a993ff358b494aeae75af5529b37af040d56052
piero <fernandezpiero11@gmail.com>
2022-04-12 23:11:12 -0500

    nuevi repository

--- src/main/java/com/example/lab_2/repository/SedeRepository.java
+++ src/main/java/com/example/lab_2/repository/SedeRepository.java
@@ -1,10 +1,10 @@
 package com.example.lab_2.repository;
 
 import com.example.lab_2.entity.Inventario;
+import com.example.lab_2.entity.Sede;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface InventarioRepository extends JpaRepository<Inventario,Integer> {
+public interface SedeRepository extends JpaRepository<Sede,Integer>{
 }
--- src/main/java/com/example/lab_2/repository/TipoRepository.java
+++ src/main/java/com/example/lab_2/repository/TipoRepository.java
@@ -1,10 +1,11 @@
 package com.example.lab_2.repository;
 
 import com.example.lab_2.entity.Inventario;
+import com.example.lab_2.entity.Tipo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface InventarioRepository extends JpaRepository<Inventario,Integer> {
+public interface TipoRepository extends JpaRepository<Tipo,Integer>{
+
 }
