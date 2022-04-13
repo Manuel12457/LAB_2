d6695ba7d19bf4f41966299f8d5da5a5e8ee4058
stephaniaguzman <stephania.guzman@pucp.edu.pe>
2022-04-13 01:59:21 -0500

    Atributos Flash Sede

--- src/main/java/com/example/lab_2/Controllers/SedeController.java
+++ src/main/java/com/example/lab_2/Controllers/SedeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
@@ -38,14 +39,17 @@ public class SedeController {
     }
 
     @PostMapping("/save")
-    public String saveSedes(@RequestParam("nombre") String nombre, Sede sede){
+    public String saveSedes(@RequestParam("nombre") String nombre, Sede sede, RedirectAttributes a){
+        if (sede.getId() == 0) { //Hay que crear
+            a.addFlashAttribute("msg", "0");
+        }
         sede.setNombreSede(nombre);
         sedeRepository.save(sede);
         return "redirect:/sede/listar";
     }
 
     @GetMapping("/editar")
-    public String editarSedes(@RequestParam("id") int id, Model model){
+    public String editarSedes(@RequestParam("id") int id, Model model, RedirectAttributes a){
         Optional<Sede> optionalSede = sedeRepository.findById(id);
         if (optionalSede.isPresent()) {
             Sede sede = optionalSede.get();
@@ -54,20 +58,25 @@ public class SedeController {
             model.addAttribute("trabajadorList", trabajadorList);
             return "sedes/editar";
         } else {
+            a.addFlashAttribute("msg", -1); //Ocurrio un error al momento de querer editar
             return "redirect:/sede/listar";
         }
     }
 
     @PostMapping("/edit")
-    public String editAreas(Sede sede){
+    public String editAreas(Sede sede, RedirectAttributes a){
+        if(sede.getId() != 0){ //Hay que actualizar
+            a.addFlashAttribute("msg", "1");
+        }
         sedeRepository.save(sede);
         return "redirect:/sede/listar";
     }
 
     @GetMapping("/borrar")
-    public String borrarSedes(@RequestParam("id") int id){
+    public String borrarSedes(@RequestParam("id") int id, RedirectAttributes a){
         Optional<Sede> optionalTrabajador = sedeRepository.findById(id);
         if (optionalTrabajador.isPresent()) {
+            a.addFlashAttribute("msg", "2"); //Marca borrada exitosamente
             sedeRepository.deleteById(id);
         }
         return "redirect:/sede/listar";
