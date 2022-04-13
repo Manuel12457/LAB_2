fb80c60c67a2b888329e766379b2eb3445fe9e25
stephaniaguzman <stephania.guzman@pucp.edu.pe>
2022-04-13 01:44:16 -0500

    CRUD Sede

--- src/main/java/com/example/lab_2/Controllers/SedeController.java
+++ src/main/java/com/example/lab_2/Controllers/SedeController.java
@@ -1,9 +1,75 @@
 package com.example.lab_2.Controllers;
 
+import com.example.lab_2.entity.Sede;
+import com.example.lab_2.entity.Trabajador;
+import com.example.lab_2.repository.SedeRepository;
+import com.example.lab_2.repository.TrabajadorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/sede")
 public class SedeController {
+
+    @Autowired
+    SedeRepository sedeRepository;
+
+    @Autowired
+    TrabajadorRepository trabajadorRepository;
+
+    @GetMapping("/listar")
+    public String listarSedes(Model model){
+        List<Sede> listaSedes = sedeRepository.findAll();
+        model.addAttribute("listaSedes",listaSedes);
+        return "sedes/lista";
+    }
+
+    @GetMapping("/newform")
+    public String newformSedes(){
+        return "sedes/newform";
+    }
+
+    @PostMapping("/save")
+    public String saveSedes(@RequestParam("nombre") String nombre, Sede sede){
+        sede.setNombreSede(nombre);
+        sedeRepository.save(sede);
+        return "redirect:/sede/listar";
+    }
+
+    @GetMapping("/editar")
+    public String editarSedes(@RequestParam("id") int id, Model model){
+        Optional<Sede> optionalSede = sedeRepository.findById(id);
+        if (optionalSede.isPresent()) {
+            Sede sede = optionalSede.get();
+            model.addAttribute("sede", sede);
+            List<Trabajador> trabajadorList = trabajadorRepository.findByIdsede(id);
+            model.addAttribute("trabajadorList", trabajadorList);
+            return "sedes/editar";
+        } else {
+            return "redirect:/sede/listar";
+        }
+    }
+
+    @PostMapping("/edit")
+    public String editAreas(Sede sede){
+        sedeRepository.save(sede);
+        return "redirect:/sede/listar";
+    }
+
+    @GetMapping("/borrar")
+    public String borrarSedes(@RequestParam("id") int id){
+        Optional<Sede> optionalTrabajador = sedeRepository.findById(id);
+        if (optionalTrabajador.isPresent()) {
+            sedeRepository.deleteById(id);
+        }
+        return "redirect:/sede/listar";
+    }
 }
--- src/main/java/com/example/lab_2/repository/TrabajadorRepository.java
+++ src/main/java/com/example/lab_2/repository/TrabajadorRepository.java
@@ -3,8 +3,16 @@ package com.example.lab_2.repository;
 import com.example.lab_2.entity.Inventario;
 import com.example.lab_2.entity.Trabajador;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TrabajadorRepository extends JpaRepository<Trabajador,String>{
+
+    List<Trabajador> findByIdsede(int idsede);
+
+    @Query(value = "select * from trabajadores where idsede = ?1",nativeQuery = true)
+    List<Trabajador> buscarTrabajadorPorIdSede(int idsede);
 }
